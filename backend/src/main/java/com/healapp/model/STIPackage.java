9beaacf51da4706e068b0bcd694dce3189d63978
nguyen van cuong <cuongnvse183645@fpt.edu.vn>
2025-07-15 09:36:42 +0700

    1

--- backend/src/main/java/com/healapp/model/STIPackage.java
+++ backend/src/main/java/com/healapp/model/STIPackage.java
@@ -1,14 +1,24 @@
 package com.healapp.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -22,7 +32,7 @@ public class STIPackage {
     @Column(name = "package_id")
     private Long packageId;
 
-    @Column(name = "package_name", nullable = false, length = 200)
+    @Column(name = "package_name", nullable = false, length = 200,columnDefinition = "NVARCHAR(255)")
     private String packageName;
 
     @Column(name = "description", columnDefinition = "NVARCHAR(MAX)")
