0fbb4d1ffcd67a4d508810fd85a7063aabaf7f0c
nguyen van cuong <cuongnvse183645@fpt.edu.vn>
2025-07-29 21:02:37 +0700

    update: logic canceled,status for payment method COD

---
+++ backend/src/main/java/com/healapp/dto/CODPaymentConfirmationRequest.java
@@ -0,0 +1,12 @@
+package com.healapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CODPaymentConfirmationRequest {
+    private String notes;
+}
